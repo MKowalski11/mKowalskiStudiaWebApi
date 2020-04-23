03e3b38af67e631eb52198aee6a5df8d58633534
MKowalski11 <gasienica98@gmail.com>
2020-04-23 23:08:06 +0200

    weryfikacja inputa przed liczeniem

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -26,34 +26,77 @@ namespace WebApi.Controllers
         [HttpGet]
         [Produces("application/json")]
         [Route("calculate")]
-        public IActionResult Get(string formula, double X)
+        public IActionResult Get(string formula, string X)
         {
             string stan = "ok";
-            if(double.TryParse())
+            double tmpDouble;
+            if (double.TryParse(X, out tmpDouble) != true)
+            {
+                var data = new
+                {
+                    status = "error",
+                    message = "Error: 'X' is not a valid double number or has not been set"
+                };
+                return Ok(data);
+            }
+            else { 
             var data = new
             {
                 status = stan,
                 formula = formula,
-                X = X
+                X = tmpDouble
             };
             return Ok(data);
+            }
         }
         [HttpGet]
         [Produces("application/json")]
         [Route("calculate/xy")]
        
-        public IActionResult Get(string formula, double from, double to, int n)
+        public IActionResult Get(string formula, string from, string to, string n)
         {
             string stan = "ok";
-            var data = new
+            double tmpDouble, tmpDouble2;
+            int tmpInt;
+            if (double.TryParse(from, out tmpDouble) != true)
             {
-                status = stan,
-                formula = formula,
-                from = from,
-                to = to,
-                n = n
-            };
-            return Ok(data);
+                var data = new
+                {
+                    status = "error",
+                    message = "Error: 'from' is not a valid double number or has not been set"
+                };
+                return Ok(data);
+            }
+            else if (double.TryParse(to, out tmpDouble2) != true)
+            {
+                var data = new
+                {
+                    status = "error",
+                    message = "Error: 'to' is not a valid double number or has not been set"
+                };
+                return Ok(data);
+            }
+            else if (Int32.TryParse(n, out tmpInt) != true)
+            {
+                var data = new
+                {
+                    status = "error",
+                    message = "Error: 'n' is not a valid integer number or has not been set"
+                };
+                return Ok(data);
+            }
+            else
+            {
+                var data = new
+                {
+                    status = stan,
+                    formula = formula,
+                    from = tmpDouble,
+                    to = tmpDouble2,
+                    n = tmpInt
+                };
+                return Ok(data);
+            }
         }
     }
 }
